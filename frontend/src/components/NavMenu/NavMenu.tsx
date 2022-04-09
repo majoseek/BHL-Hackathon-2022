2fc167b49149b1e37ef681f90b7e626fec4ff015
majoseek <matuslaw.mati@gmail.com>
2022-04-09 03:34:09 +0200

    Landing page update

--- frontend/src/components/NavMenu/NavMenu.tsx
+++ frontend/src/components/NavMenu/NavMenu.tsx
@@ -1,13 +1,30 @@
+//@ts-nocheck
+import { useState } from "react";
+import { Sidebar } from "primereact/sidebar";
 import { Button } from "primereact/button";
-import React from "react";
-class ButtonDemo extends React.Component {
-    render() {
-        return (
-            <div className="button-demo">
-                <div className="card">
-                    <Button />
-                </div>
+import LogoName from "./logo_name.png";
+import "./styles.css";
+const NavMenu = () => {
+    const [visibleTop, setVisibleTop] = useState(false);
+    return (
+        <div>
+            <div className="card">
+                <Sidebar
+                    visible={visibleTop}
+                    position="top"
+                    onHide={() => setVisibleTop(false)}
+                >
+                    <h3>Top Sidebar</h3>
+                </Sidebar>
+
+                <img src={LogoName} alt="Logo" />
+                <Button
+                    icon="pi pi-arrow-down"
+                    onClick={() => setVisibleTop(true)}
+                    className="mr-2 sidebar-icon"
+                />
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
+export default NavMenu;
--- frontend/src/views/ShoppingList/table/ProductsTableDictionary.tsx
+++ frontend/src/views/ShoppingList/table/ProductsTableDictionary.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, {useEffect, useState} from 'react';
 import {DataTable} from 'primereact/datatable';
 import {Column} from 'primereact/column';
