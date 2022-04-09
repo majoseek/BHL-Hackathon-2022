3a7021167a13f001886aa58e62ace911f016702f
wkolodz2 <01158744@pw.edu.pl>
2022-04-09 11:54:47 +0200

    Add tags for ProductInfoDTOS

--- backend/src/main/java/com/example/backend/product/ProductInfoDTO.java
+++ backend/src/main/java/com/example/backend/product/ProductInfoDTO.java
@@ -1,6 +1,7 @@
 package com.example.backend.product;
 
 import java.util.List;
+import java.util.Objects;
 
 public class ProductInfoDTO {
     private final Long id;
@@ -10,6 +11,7 @@ public class ProductInfoDTO {
     private final String grammage;
     private final String imgURL;
     private final Integer averagePrice;
+    private List<String> tags;
 
     //[class java.lang.Long, class java.lang.String, class java.lang.String, class java.lang.String, class java.lang.String
     public ProductInfoDTO(Long id, String name, String eanCode, String manufacturer, String grammage, String imgURL, Integer averagePrice) {
@@ -48,5 +50,26 @@ public class ProductInfoDTO {
 
     public Integer getAveragePrice() {
         return averagePrice;
+    }
+
+    public List<String> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductInfoDTO that = (ProductInfoDTO) o;
+        return id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
