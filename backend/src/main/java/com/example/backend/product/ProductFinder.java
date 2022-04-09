380eaff7c4addd95460ef996cb3a719c73bac40c
wkolodz2 <01158744@pw.edu.pl>
2022-04-09 11:01:42 +0200

    Changes...

--- backend/src/main/java/com/example/backend/product/ProductFinder.java
+++ backend/src/main/java/com/example/backend/product/ProductFinder.java
@@ -11,10 +11,7 @@ import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import static com.example.backend.availableProduct.QAvailableProduct.availableProduct;
 import static com.example.backend.product.QProduct.product;
@@ -26,7 +23,7 @@ import static com.querydsl.core.types.Projections.constructor;
 
 @Service
 public class ProductFinder {
-    private final Double MAX_DISTANCE = 100 * 1000D;
+    private final Double MAX_DISTANCE = 50 * 1000D;
 
     @PersistenceContext
     private EntityManager entityManager;
@@ -38,29 +35,11 @@ public class ProductFinder {
     }
 
     ProductInfoDTO getProductInfoById(Long id) {
-        return new JPAQuery<>(entityManager)
-                .from(product)
-                .where(product.id.in(id))
-                .select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL))
-                .fetchFirst();
+        return new JPAQuery<>(entityManager).from(product).where(product.id.in(id)).select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL)).fetchFirst();
     }
 
     List<ProductInfoDTO> getProducts() {
-//        products.forEach(product -> {
-//            List<StockInfo> stockAvailability = new JPAQuery<>(entityManager).from(availableProduct)
-//                    .join(availableProduct.shop, shop)
-//                    .join(availableProduct.product, QProduct.product)
-//                    .where(availableProduct.product.id.eq(product.getId()))
-//                    .select(constructor(StockInfo.class, shop.id, shop.name, shop.longitude, shop.latitude, availableProduct.priceInGr, availableProduct.quantity))
-//                    .fetch();
-//            product.setStockAvailability(stockAvailability);
-//        });
-        return new JPAQuery<>(entityManager)
-                .from(product)
-                .leftJoin(product.availableProducts, availableProduct)
-                .select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue()))
-                .groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL)
-                .fetch();
+        return new JPAQuery<>(entityManager).from(product).leftJoin(product.availableProducts, availableProduct).select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue())).groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL).fetch();
     }
 
     List<ProductStockInfoDTO> getStockAvailibity(Set<Long> productIds, Double userLatitude, Double userLongtitude, Double QEfficient) {
@@ -68,12 +47,7 @@ public class ProductFinder {
     }
 
     private Map<Long, Set<StockInfo>> getShopsWithProducts(Set<Long> productsIds) {
-        return new JPAQuery<>(entityManager).from(availableProduct)
-                .join(availableProduct.shop, shop)
-                .join(availableProduct.product, product)
-                .where(product.id.in(productsIds))
-                .groupBy(shop.id, shop.name, shop.longitude, product.id, product.EANCode, product.grammage, product.name, product.manufacturer, product.imgURL, shop.latitude, availableProduct.priceInGr, availableProduct.quantity)
-                .transform(groupBy(shop.id).as(GroupBy.set(constructor(StockInfo.class, constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue()),  availableProduct.priceInGr, availableProduct.quantity))));
+        return new JPAQuery<>(entityManager).from(availableProduct).join(availableProduct.shop, shop).join(availableProduct.product, product).where(product.id.in(productsIds)).groupBy(shop.id, shop.name, shop.longitude, product.id, product.EANCode, product.grammage, product.name, product.manufacturer, product.imgURL, shop.latitude, availableProduct.priceInGr, availableProduct.quantity).transform(groupBy(shop.id).as(GroupBy.set(constructor(StockInfo.class, constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue()), availableProduct.priceInGr, availableProduct.quantity))));
     }
 
     private List<ProductStockInfoDTO> getProductStockInfo(Set<Long> productIds, Double userLatitude, Double userLongtitude, Double QCoefficient) {
@@ -83,34 +57,44 @@ public class ProductFinder {
         shopsWithProducts.forEach((shopId, stockInfos) -> {
             ShopInfoDTO shopInfo = shopFinder.getShopInfoById(shopId);
             double distanceInMeters = DistanceService.getDistance(userLatitude, userLongtitude, shopInfo.latitude(), shopInfo.longitude());
-//            if (distanceInMeters < MAX_DISTANCE) {
+            if (distanceInMeters < MAX_DISTANCE) {
                 List<ParamsInputData> productsInfos = stockInfos.stream().map((it) -> new ParamsInputData(it.price(), it.productInfoDTO().getAveragePrice(), it.quantity())).toList();
                 Integer missingProducts = productIds.size() - stockInfos.size();
-                productStockInfoDTOS.add(new ProductStockInfoDTO(shopInfo, distanceInMeters, stockInfos, ShopScoreGenerator.generateScore(distanceInMeters, missingProducts, productsInfos, QCoefficient)));
-
-//            }
+                productStockInfoDTOS.add(new ProductStockInfoDTO(shopInfo, distanceInMeters, stockInfos, ShopScoreGenerator.generateScore(distanceInMeters, missingProducts, productsInfos, QCoefficient), missingProducts));
+            }
         });
         return productStockInfoDTOS;
     }
 
 
-
     List<ProductInfoDTO> getProductsByTag(String providedTag) {
-        return new JPAQuery<>(entityManager).from(product)
-                .join(product.tags, tag)
-                .leftJoin(product.availableProducts, availableProduct)
-                .select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue()))
-                .groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL)
-                .fetch();
+        return new JPAQuery<>(entityManager).from(product).join(product.tags, tag).leftJoin(product.availableProducts, availableProduct).select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue())).groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL).fetch();
     }
 
     List<ProductInfoDTO> getProductsByName(String providedName) {
-        return new JPAQuery<>(entityManager).from(product)
-                .where(product.name.like("%" + providedName + "%"))
-                .leftJoin(product.availableProducts, availableProduct)
-                .select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue()))
-                .groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL)
-                .fetch();
+        return new JPAQuery<>(entityManager).from(product).where(product.name.like("%" + providedName + "%")).leftJoin(product.availableProducts, availableProduct).select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue())).groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL).fetch();
     }
 
+    List<ProductInfoDTO> getProductsByNames(List<String> providedNames) {
+        return new JPAQuery<>(entityManager).from(product).where(product.name.in(providedNames)).leftJoin(product.availableProducts, availableProduct).select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue())).groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL).fetch();
+    }
+
+    //Get products by list of tags
+    List<ProductInfoDTO> getProductsByTags(List<String> providedTags) {
+        return new JPAQuery<>(entityManager).from(product).join(product.tags, tag).where(tag.name.in(providedTags)).leftJoin(product.availableProducts, availableProduct).select(constructor(ProductInfoDTO.class, product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL, availableProduct.priceInGr.avg().intValue())).groupBy(product.id, product.name, product.EANCode, product.manufacturer, product.grammage, product.imgURL).fetch();
+    }
+
+
+    public List<ProductInfoDTO> getProductsByNameOrTag(String name, String tag) {
+
+        List<ProductInfoDTO> productsByName = getProductsByName(name);
+        List<ProductInfoDTO> productsByTag = getProductsByTag(tag);
+
+        List<ProductInfoDTO> products = new LinkedList<>();
+        products.addAll(productsByName);
+        products.addAll(productsByTag);
+
+        return products;
+
+    }
 }
---
+++ backend/src/main/java/com/example/backend/product/RecipeParser.java
@@ -0,0 +1,50 @@
+package com.example.backend.product;
+
+import com.example.backend.tag.TagFinder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
+
+@Component
+public class RecipeParser {
+
+    private final ProductFinder productFinder;
+    private final TagFinder tagFinder;
+
+    public RecipeParser(ProductFinder productFinder, TagFinder tagFinder) {
+        this.productFinder = productFinder;
+        this.tagFinder = tagFinder;
+    }
+
+    private List<String> splitProductsAndQuantity(String recipe) {
+        return recipe.lines().map(line -> line.split("-", 2)[0].trim()).toList();
+    }
+
+    private List<ProductInfoDTO> getAppropriateProducts(List<String> products) {
+        List<ProductInfoDTO> productInfoDTOS = productFinder.getProductsByNames(products);
+        if (!productInfoDTOS.isEmpty()) return productInfoDTOS;
+        List<String> tags = tagFinder.getTagsAlike(products);
+        return productFinder.getProductsByTags(tags);
+    }
+
+
+    List<ProductInfoDTO> getProductsByFile(MultipartFile file) throws IOException {
+        InputStream inputStream = file.getInputStream();
+        List<ProductInfoDTO> productInfoDTOS = new java.util.ArrayList<>(Collections.emptyList());
+        new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                .lines()
+                .forEach((it) -> {
+                    productInfoDTOS.addAll(getAppropriateProducts(splitProductsAndQuantity(it)));
+                });
+        return productInfoDTOS;
+    }
+
+
+}
